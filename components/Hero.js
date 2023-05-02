5dd04c74eb4e876d2a67d51e9e304795e0b76cf2
Brandon Thomas <bran_thom@yahoo.com>
2023-05-02 13:16:00 -0500

    airbnb close React static

---
+++ components/Hero.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Hero(){
+    return (
+        <main>
+        <img className = "hero--pic"src = "./images/photo-grid.png"/>
+        <h1 className="hero--text">Online Experiences</h1>
+        <p className="hero--text" className="paragraph">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+</p>
+        </main>
+    )
+}
